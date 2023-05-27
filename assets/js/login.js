248080813b4518dbae1919b7eff7c877e8a50d00
一方小芥 <qin2589634223@163.com>
2023-05-28 01:48:20 +0800

    三次骨架完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,4 @@
 
 $(document).ready(function() {
-  // 注册按钮事件
-  // $('#register_account').on('click', () => {
-  //   $('#reg_page').hide()
-  //   $('#login_page').show()
-  // })
+  console.dir(layui)
 })
